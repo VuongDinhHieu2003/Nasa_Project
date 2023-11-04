f67caf7e88068903e886a5351bc2dc85d854ac42
Vuong Dinh Hieu <vuongdinhhieu2003@gmail.com>
2023-11-05 04:05:34 +0700

    first commit

---
+++ Nasa_21110181/server/src/routes/launches/launches.controller.js
@@ -0,0 +1,46 @@
+const { 
+    getAllLaunches,
+    addNewLaunch,
+    existLaunchWithID,
+    abortLaunchByID,
+ } = require('../../models/launches.model');
+
+function httpGetAllLaunches(req, res) {  
+    return res.status(200).json(getAllLaunches());
+}
+
+function httpAddNewLaunch (req, res) {  
+    const launch = req.body;
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
+    launch.launchDate = new Date(launch.launchDate);
+    if(isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
+    addNewLaunch(launch);
+    return res.status(201).json(launch);
+}
+
+function httpAbortLaunch (req, res) {  
+    const launchID = Number(req.params.id);
+
+    if(!existLaunchWithID(launchID)){
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
+    // if launch exist
+    const aborted = abortLaunchByID(launchID);
+    return res.status(200).json(aborted);
+}
+
+module.exports = {
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch,
+}
